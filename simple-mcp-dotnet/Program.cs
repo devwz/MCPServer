63d716eb876bf7187eb27a712c4f1c609013c6a1
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2026-04-17 00:26:36 -0300

    Add simple-mcp-dotnet project

---
+++ simple-mcp-dotnet/Program.cs
@@ -0,0 +1,24 @@
+﻿using ModelContextProtocol.Server;
+using ModelContextProtocol.Protocol;
+
+var server = McpServer.Create(new StdioServerTransport("simple-mcp-dotnet"), new McpServerOptions
+{
+    ServerInfo = new Implementation
+    {
+        Name = "simple-mcp-dotnet",
+        Version = "1.0.0"
+    },
+    ToolCollection = new McpServerPrimitiveCollection<McpServerTool>()
+    {
+        McpServerTool.Create(
+            () => "Hello, World",
+            new McpServerToolCreateOptions
+            {
+                Name = "hello_world",
+                Description = "Returns Hello, World"
+            }
+        )
+    }
+});
+
+await server.RunAsync();
