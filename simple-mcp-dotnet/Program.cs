6ef1083120d3b78c3c3acae4f7398d8b6ac243fd
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2026-05-06 00:09:47 -0300

    Add new server tools

--- simple-mcp-dotnet/Program.cs
+++ simple-mcp-dotnet/Program.cs
@@ -1,7 +1,9 @@
 ﻿using ModelContextProtocol.Server;
 using ModelContextProtocol.Protocol;
 
-var server = McpServer.Create(new StdioServerTransport("simple-mcp-dotnet"), new McpServerOptions
+Random random = new ();
+
+McpServer server = McpServer.Create(new StdioServerTransport("simple-mcp-dotnet"), new McpServerOptions
 {
     ServerInfo = new Implementation
     {
@@ -11,11 +13,27 @@ var server = McpServer.Create(new StdioServerTransport("simple-mcp-dotnet"), new
     ToolCollection = new McpServerPrimitiveCollection<McpServerTool>()
     {
         McpServerTool.Create(
-            () => "Hello, World",
+            () => $"Hello, world!",
             new McpServerToolCreateOptions
             {
                 Name = "hello_world",
-                Description = "Returns Hello, World"
+                Description = "Return a greeting message"
+            }
+        ),
+        McpServerTool.Create(
+            (int number) => random.Next(1, number + 1),
+            new McpServerToolCreateOptions
+            {
+                Name = "get_random_number",
+                Description = "Get a random number between 1 and the given number"
+            }
+        ),
+        McpServerTool.Create(
+            (int number) => number % 2 == 0 ? "even" : "odd",
+            new McpServerToolCreateOptions
+            {
+                Name = "check_even_or_odd",
+                Description = "Check if a number is even or odd"
             }
         )
     }
